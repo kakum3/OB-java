ca4b688de796a93fddcf8d1bd08c5d33d05b40b3
kakum3 <104210758+kakum3@users.noreply.github.com>
2022-12-16 12:08:13 +0100

    ejercicio1

---
+++ src/EjerciciosTema1/Main.java
@@ -0,0 +1,21 @@
+package EjerciciosTema1;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        short variable1 = 2;
+        System.out.println (variable1);
+        int variable2 = 15;
+        System.out.println (variable2);
+        long variable3 = 58945570;
+        System.out.println (variable3);
+        double variable4 = 0.34;
+        System.out.println (variable4);
+        boolean variable5 = true;
+        System.out.println (variable5);
+        String variable6 = "Texto";
+        System.out.println (variable6);
+
+    }
+}
