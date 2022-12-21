d47820d5c70124d895f86ee3d50319fa01ce8770
kakum3 <104210758+kakum3@users.noreply.github.com>
2022-12-21 09:39:31 +0100

    ejercicio3

---
+++ src/EjerciciosTema3/Main.java
@@ -0,0 +1,12 @@
+package EjerciciosTema3;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] nombres = new String[]{" Juan ", " Pedro "," Maria "," Jesús"};
+            for (int i = 0; i < nombres.length; i++) {
+                System.out.println (nombres[i]);
+            }
+        }
+
+    }
+
